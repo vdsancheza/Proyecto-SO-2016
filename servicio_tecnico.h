3beb8507ffabc213d091feb08711ff97ddd263f7
Victor Tortolero <victormtortolero@gmail.com>
2016-07-23 16:01:44 -0400

    Added library and test files

---
+++ servicio_tecnico.h
@@ -0,0 +1,19 @@
+#ifndef SERVICIO_TECNICO_H
+#define SERVICIO_TECNICO_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* 
+	Modulo hecho por:
+	Mura Javier		21.456.202
+	Labbe Andres		
+	Capriles Oswaldo	
+	Victor Tortolero	24.569.609
+*/
+
+
+
+
+
+#endif
---
+++ test.c
@@ -0,0 +1,11 @@
+#include "servicio_tecnico.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+	printf("Hello Peasants\n");
+
+	return 0;
+}
