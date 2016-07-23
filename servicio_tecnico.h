28328ffc9d1934dfbc3118c1f7396b5dcd30d312
Oswaldo Capriles <oswald.capriles46@gmail.com>
2016-07-23 16:57:10 -0430

    Agregado makefile, wink wink

--- servicio_tecnico.h
+++ servicio_tecnico.h
@@ -3,15 +3,21 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
-/* 
+#include <pthread.h>
+#include <semaphore.h>
+/*
 	Modulo hecho por:
-	Mura Javier		21.456.202
+<<<<<<< HEAD
+	Mura Javier			21.456.202
 	Labbe Andres		22.728.231
 	Capriles Oswaldo	26.026.458
 	Victor Tortolero	24.569.609
+	//soy master?
+
+	Mura Javier		21.456.202
+	Labbe Andres
+	Capriles Oswaldo
+	Victor Tortolero	24.569.609
 */
-
-
 
 #endif
