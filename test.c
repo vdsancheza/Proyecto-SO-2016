84e4c4bb0aa7bf2909caba706c669c4e7d8a6aaa
naughty-code <javier.murar@gmail.com>
2016-07-23 20:26:57 -0430

    added things to test

--- test.c
+++ test.c
@@ -2,10 +2,42 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <time.h>
 
 
-int main() {
-	printf("Hello Peasants\n");
+typedef struct{
+	sem_t fallo_de_energia;
+} Shared;
 
-	return 0;
+void* servicioTecnico(void *);
+
+int main() //Loop principal
+{
+	pthread_t IT;
+	Shared data;
+	sem_init(&data.fallo_de_energia, 1, 0);
+	printf("Hello world!\n");
+	srand(time(NULL));
+	pthread_create(&IT, NULL, servicioTecnico, (void *)&data);
+
+	while(1){
+		if(rand() % 2 < 0.05){
+			sem_post(&data.fallo_de_energia);
+	    	}
+	    	sleep(1);
+    	}
+    sleep(5);
+    return 0;
 }
+
+void* servicioTecnico(void * data){
+    //variables compartidas
+    Shared *shared = (Shared*) (data);
+	while(1){
+		sem_wait(&shared->fallo_de_energia);
+        	printf("Hubo un fallo de energia!\n");
+	}
+}
+
