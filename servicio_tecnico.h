3bd3fb5b6d93fb492579cbc14cdc9f9af7b1d717
Paharo <paharo@Paharo>
2016-07-23 20:40:00 -0400

    soy contibutor master sql

--- servicio_tecnico.h
+++ servicio_tecnico.h
@@ -6,12 +6,11 @@
 
 /* 
 	Modulo hecho por:
-	Mura Javier		21.456.202
+	Mura Javier			21.456.202
 	Labbe Andres		22.728.231
 	Capriles Oswaldo	26.026.458
 	Victor Tortolero	24.569.609
 */
-
-
+	//soy master?
 
 #endif
